b5a7c20835b59e2f3fc9ca4129d3f3d4b2f86f57
Janmesh Singh <68192323+janmeshjs@users.noreply.github.com>
2023-10-04 22:58:28 +0530

    Update Laststoneweight.java

--- Java/Laststoneweight.java
+++ Java/Laststoneweight.java
@@ -1,21 +1,24 @@
-import java.util.Collections;
 import java.util.*;
+import java.util.Collections;
 
 public class Laststoneweight {
     public int lastStoneWeight(int[] stones) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int x: stones){
+        for (int x : stones) {
             pq.add(x);
         }
-        int st=1;
-        while(st>0 && pq.size()>1){
-            int st1= pq.poll();
+        int st = 1;
+        while (st > 0 && pq.size() > 1) {
+            int st1 = pq.poll();
             int st2 = pq.poll();
-            if(st1==st2) continue;
-            else pq.add(st1-st2);
+            if (st1 == st2)
+                continue;
+            else
+                pq.add(st1 - st2);
         }
-        if(pq.size()==1) return pq.poll();
-        else return 0;
+        if (pq.size() == 1)
+            return pq.poll();
+        else
+            return 0;
     }
 }
-
