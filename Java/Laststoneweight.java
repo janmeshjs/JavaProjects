7b8f9f997745c346a0eccfd6f1257b4945a23c7b
janmesh Singh <Janmesh.js@gmail.com>
2023-10-04 22:37:49 +0530

    Testing

---
+++ Java/Laststoneweight.java
@@ -0,0 +1,22 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Laststoneweight {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int x: stones){
+            pq.add(x);
+        }
+        int st=1;
+        while(st>0 && pq.size()>1){
+            int st1= pq.poll();
+            int st2 = pq.poll();
+            if(st1==st2) continue;
+            else pq.add(st1-st2);
+        }
+        
+        if(pq.size()==1) return pq.poll();
+        else return 0;
+    }
+}
+
